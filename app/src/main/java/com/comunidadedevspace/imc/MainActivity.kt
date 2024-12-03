b6fadd7c0ea816be2f17a99e0f46fc9913f5fead
diasdias <icarodias20pt@gmail.com>
2024-12-03 12:02:44 +0000

    commit edttext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,32 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar componente Edittext
+            //Criar variavel e associar(=) o componente UI<Edittext>
+        // Recuperar bota da tela
+        // Colocar acao no bota com setOnClickListener
+        // Recuperar texto digitado em edtPeso
+
+        var edtPeso = findViewById<TextInputEditText>(R.id.edtpeso)
+        var edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        var btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener{
+        var peso = edtPeso.text
+        var altura = edtAltura.text
+
+        println("Icaro pesa " + peso + " e mede " + altura)
+        }
+
+
     }
 }
